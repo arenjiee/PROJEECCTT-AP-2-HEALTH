bd2763191d526183471ea28879572a94d6164ad9
arenjiee <moskovwtf@gmail.com>
2025-11-10 15:07:46 +0800

    ADD: show

---
+++ show.py
@@ -0,0 +1,17 @@
+def greeting():
+    print("=" * 107)
+    print(f'{"SELAMAT DATANG DI AP-2-HEALTH !!":^105}')
+    print("=" * 107)
+    print(f'|{"Program ini ngajak kamu bukan cuma tahu pentingnya sehat, tapi juga beneran hidup sehat tiap hari":^105}|')
+    print(f'|{"From Awareness to Action, AP-2-Health, your journey to a balanced life":^105}|')
+    print("=" * 107)
+
+def menu():
+    print("\n1. 📏 Hitung BMI ")
+    print("2. 📊 SmartFat Calculator")
+    print("3. ✅ HealthCheck")
+    print("4. 🎯 Set Health Mission")
+    print("5. 📈 Health Progress")
+    print("6. 😊 How Are You Today?")
+    print("7. 👁️‍🗨️ EyeCheck")
+    print("8. 👋 Exit")
